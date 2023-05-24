bf898d2080be6239e735cf64151cb89edcb18e9b
alech <hamiltonalec@gmail.com>
2023-05-24 17:10:50 +0100

    feat: imported display font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,15 @@
 	import "../app.css";
 </script>
 
-<main>
-	<slot />
-</main>
+<header class="flex flex-col">
+	<h1 class="font-display">Alec Hamilton</h1>
+  <h2>Full Stack Developer</h2>
+</header>
+
+<slot />
+
+<style lang="postcss">
+	:global(body) {
+		background-color: theme(colors.bg);
+	}
+</style>
