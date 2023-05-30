e0aaa3c00411242169fb99955733df76fb826bb3
alech <hamiltonalec@gmail.com>
2023-05-30 16:57:30 +0100

    feat: skills & about me styled

---
+++ src/data/skills.js
@@ -0,0 +1,14 @@
+export const skills = [
+  {
+    heading: "Frontend",
+    list: ["React", "NextJS", "TypeScript", "TailwindCSS"],
+  },
+  {
+    heading: "Frontend",
+    list: ["React", "NextJS", "TypeScript", "TailwindCSS"],
+  },
+  {
+    heading: "Frontend",
+    list: ["React", "NextJS", "TypeScript", "TailwindCSS"],
+  },
+];
